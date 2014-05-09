48778564dcd1daff731e708f329a2e213fbb298e
Lennart de Wit <lenn.dewit@gmail.com>
2014-05-09 12:17:11 +0200

    Setting up assignment

---
+++ js/test.js
@@ -0,0 +1,9 @@
+$(function () {
+	    $(".box").animate({
+       left: '600'
+           }, { duration: 1000, queue: true });
+    $(".box").animate({
+       top: '600'
+    }, { duration: 1000, queue: true });
+});
+
